f88b8cfc098ba71ba0f85e013e73789adbc47391
kiralukichev <kirill.lukichyov@mail.ru>
2025-10-27 21:37:26 +0300

    fixed values

--- src/project.py
+++ src/project.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 retail = pd.read_csv('C:/VsCode/venv/retail-transaction-analysis/data/data.csv', encoding= 'ISO-8859-1')
 transaction_data = pd.read_csv('C:/VsCode/venv/retail-transaction-analysis/data/transaction_data.csv')
+
 retail['Date'] = pd.to_datetime(retail.InvoiceDate)
 transaction_data['date'] = pd.to_datetime(transaction_data.date)
 transaction_data['minute'] = transaction_data['date'].dt.minute
@@ -27,8 +28,7 @@ cancellation_rate = (cancelled_count / total_orders) * 100
 retail_uniq = retail.drop(retail[retail['InvoiceNo'].str.startswith('C')].index)
 
 # Анализ покупок наиболее активных пользователей из Германии. 
-# (Ваши коллеги уже расчитали, что этот порог — 80-й процентиль.)
-
+# (Коллеги уже расчитали, что этот порог — 80-й процентиль.)
 uniq_ger = retail_uniq.query('Country == "Germany"') \
     .groupby('CustomerID', as_index=False) \
     .agg({'InvoiceNo':'nunique'})
@@ -40,6 +40,47 @@ top_customers = uniq_ger.query('InvoiceNo > @proc_80')['CustomerID']
 # Смотрим на табличку только по этим подльзователям.
 top_retail_germany = retail_uniq[retail_uniq.CustomerID.isin(top_customers)]
 
+# Расчет RFM
+def calculate_rfm_metrics(df):
+    #Расчет RFM-метрик для сегментации клиентов#
+    
+    # Фильтруем только успешные транзакции (без отмен)
+    successful_df = df[~df['InvoiceNo'].str.startswith('C', na=False)]
+    successful_df = successful_df[successful_df['Quantity'] > 0]
+    
+    # Recency
+    current_date = successful_df['Date'].max()
+    recency = successful_df.groupby('CustomerID') \
+        .agg ({'Date':'max'}) \
+        .reset_index()
+    recency['Recency'] = (current_date - recency['Date']).dt.days
+    
+    # Frequency
+    frequency = successful_df.groupby('CustomerID') \
+        .agg({'InvoiceNo':'nunique'}) \
+        .reset_index()
+    frequency.rename(columns={'InvoiceNo': 'Frequency'}, inplace=True)
+    
+    # Monetary
+    successful_df['TotalValue'] = successful_df['Quantity'] * successful_df['UnitPrice']
+    monetary = successful_df.groupby('CustomerID') \
+        .agg({'TotalValue':'sum'}) \
+        .reset_index()
+    monetary.rename(columns={'TotalValue': 'Monetary'}, inplace=True)
+    
+    # Объединяем
+    rfm = recency.merge(frequency, on='CustomerID').merge(monetary, on='CustomerID')
+    
+    # RFM-баллы
+    rfm['R_Score'] = pd.qcut(rfm['Recency'], 5, labels=[5,4,3,2,1])
+    rfm['F_Score'] = pd.qcut(rfm['Frequency'], 5, labels=[1,2,3,4,5])
+    rfm['M_Score'] = pd.qcut(rfm['Monetary'], 5, labels=[1,2,3,4,5])
+    
+    return rfm
+
+# Применяем к данным по Германии
+german_rfm = calculate_rfm_metrics(top_retail_germany)
+
 top_products = top_retail_germany.query('StockCode != "POST"') \
     .groupby('StockCode', as_index=False) \
     .agg({'InvoiceNo':'count'}) \
@@ -48,12 +89,7 @@ top_products = top_retail_germany.query('StockCode != "POST"') \
 #print(f"Вывод: Топ-5 товаров среди премиальных клиентов: {list(top_products.index)}. \
 #Эти товары стоит включить в персонализированные предложения и рекламные кампании.")
 
-# Вернемся к анализу датафрейма retail. Вам нужно найти 5 наиболее крупных по выручке заказов. 
-# Для этого сначала посчитайте сумму покупки для каждой транзакции, т.е. создайте колонку Revenue с суммой покупки, используя колонки Quantity и UnitPrice. 
-# Потом для каждого заказа (см.колонку InvoiceNo) суммируйте выручку всех входящих в него транзакций — это будет колонка TotalRevenue. 
-# Отсортируйте записи в порядке убывания TotalRevenue. 
-# В качестве ответа укажите топ-5 заказов (см.колонку InvoiceNo) по сумме заказа (через запятую с пробелом, в том же порядке).
-
+# 5 наиболее крупных по выручке заказов. 
 retail_uniq['Revenue'] = retail_uniq['Quantity']*retail_uniq['UnitPrice']
 top_orders = retail_uniq.groupby('InvoiceNo', as_index=False) \
     .agg({'Revenue':'sum'}) \
@@ -86,44 +122,7 @@ peak_minute = trans_per_min.sum(axis=1).idxmax()
 peak_activity = trans_per_min.sum(axis=1).max()
 avg_activity = trans_per_min.sum(axis=1).mean()
 growth_factor = peak_activity / avg_activity 
-#print(f"Вывод: Обнаружен аномальный всплеск активности в {peak_minute}-ю минуту: +{growth_factor:.1f}x к среднему значению. \
-#Это напрямую коррелирует с публикацией рекламы блогером и демонстрирует мгновенный отклик аудитории.")
-
-# Расчет RFM
-
-def calculate_rfm_metrics(df):
-    """Расчет RFM-метрик для сегментации клиентов"""
-    
-    # Фильтруем только успешные транзакции (без отмен)
-    successful_df = df[~df['InvoiceNo'].str.startswith('C', na=False)]
-    successful_df = successful_df[successful_df['Quantity'] > 0]
-    
-    # Recency
-    current_date = successful_df['Date'].max()
-    recency = successful_df.groupby('CustomerID')['Date'].max().reset_index()
-    recency['Recency'] = (current_date - recency['Date']).dt.days
-    
-    # Frequency
-    frequency = successful_df.groupby('CustomerID')['InvoiceNo'].nunique().reset_index()
-    frequency.rename(columns={'InvoiceNo': 'Frequency'}, inplace=True)
-    
-    # Monetary
-    successful_df['TotalValue'] = successful_df['Quantity'] * successful_df['UnitPrice']
-    monetary = successful_df.groupby('CustomerID')['TotalValue'].sum().reset_index()
-    monetary.rename(columns={'TotalValue': 'Monetary'}, inplace=True)
-    
-    # Объединяем
-    rfm = recency.merge(frequency, on='CustomerID').merge(monetary, on='CustomerID')
-    
-    # RFM-баллы
-    rfm['R_Score'] = pd.qcut(rfm['Recency'], 5, labels=[5,4,3,2,1])
-    rfm['F_Score'] = pd.qcut(rfm['Frequency'], 5, labels=[1,2,3,4,5])
-    rfm['M_Score'] = pd.qcut(rfm['Monetary'], 5, labels=[1,2,3,4,5])
-    
-    return rfm
-
-# Применяем к данным по Германии
-german_rfm = calculate_rfm_metrics(top_retail_germany)
+#print(f"Вывод: Обнаружен аномальный всплеск активности в {peak_minute}-ю минуту: +{growth_factor:.1f}x к среднему значению.")
 
 # Для просмотра значений и таблиц:
 print()
